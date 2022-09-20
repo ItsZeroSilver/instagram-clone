4fb2e0729d7d73d0fd9698b5f4fccf1700963e95
ItsZeroSliver <ga417@naver.com>
2022-09-20 23:28:32 +0900

    this is commit

--- src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
+++ src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PhotogramStartApplication {
-
+	//주석 테스트 commit
 	public static void main(String[] args) {
 		SpringApplication.run(PhotogramStartApplication.class, args);
 	}
